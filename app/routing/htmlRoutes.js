094a5ffa78ee74586bb698f1a5a1ad33feaf156d
pablackhawk <l.tirtarahardja@gmail.com>
2018-01-23 00:45:02 -0800

    modified html routes, moved public folder, app now works

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ const path = require('path')
 module.exports = function(app) {
   // Home page
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
+    res.sendFile(path.join(__dirname, '/../public/home.html'))
   })
 
   // Survey page
   app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/survey.html'))
+    res.sendFile(path.join(__dirname, '/../public/survey.html'))
   })
 }
